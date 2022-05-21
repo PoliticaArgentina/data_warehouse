7c8dae2c244c9faeac6bb83b924d13eed9875de0
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2022-05-21 19:54:03 -0300

    fix icg download script

--- .github/source_R/icg.R
+++ .github/source_R/icg.R
@@ -68,7 +68,7 @@ df <- base::suppressWarnings(base::try(default <-  haven::read_dta(icg_file) %>%
     df <- data.frame(msje =  glue::glue("Fail to download data. Source is not available - {format(Sys.time(), 
     '%a %b %d %X %Y')}"))
     
-    write.csv(df, file = glue::glue("legislAr/data_check/check_{Sys.Date()}.csv"))
+    write.csv(df, file = glue::glue("opinar/data_check/check_{Sys.time()}.csv"))
     
     
     } else {
